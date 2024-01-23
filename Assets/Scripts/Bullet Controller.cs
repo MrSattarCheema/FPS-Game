987421976ae4debd6a2103f98a4213269bb4746a
Abdul Sattar <abdulsattar.cheemag@gmail.com>
2024-01-23 17:58:14 +0500

    enemy fire and animation

--- Assets/Scripts/Bullet Controller.cs
+++ Assets/Scripts/Bullet Controller.cs
@@ -8,6 +8,7 @@ public class BulletController : MonoBehaviour
     public float movingSpeed, lifeTime;
     public ParticleSystem lasertParticleEffect;
     public int demagePower = 1;
+    public bool demageEnemy, demagePlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,9 +27,17 @@ public class BulletController : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag == "Enemy" && demageEnemy)
         {
             other.gameObject.GetComponent<EnemyHealth>().DamageEnemy(demagePower);
+        }
+        if (other.gameObject.tag == "Player" && demagePlayer)
+        {
+            //Destroy(other.gameObject);
+        }
+        if ((other.tag == "Player" && demageEnemy) || (other.tag == "Enemy" && demagePlayer))
+        {
+            return;
         }
         Destroy(this.gameObject);
         Instantiate(lasertParticleEffect, transform.position + (transform.forward * (-movingSpeed * Time.deltaTime)), transform.rotation);
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     public NavMeshAgent navAgent;
-    public float keepChasingTime = 5f, minDistanceForChasing = 10f, minDistanceForLose = 15f, distanceToStop = 2f;
-    private bool isChasing;
+    public float keepChasingTime = 5f, minDistanceForChasing = 10f, minDistanceForLose = 15f, distanceToStop = 2f, fireRate;
+    private bool isChasing, canFire = true;
     private Vector3 targetPointPosition, startPosition;
-    private float chaseTimeCounter;
+    private float chaseTimeCounter, fireCounter, burstLimiter = 0;
+    public GameObject bullet;
+    public Transform bulletPoint;
+    public Animator enemyAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,15 @@ public class EnemyController : MonoBehaviour
     {
         targetPointPosition = PlayerController.instance.transform.position;
         targetPointPosition.y = transform.position.y;
+
         if (!isChasing)
         {
             if (Vector3.Distance(transform.position, targetPointPosition) < minDistanceForChasing)
             {
                 isChasing = true;
+                fireCounter = 1; // wait 1s before starting firing every time when start chasing Player
             }
+
             if (chaseTimeCounter > 0)
             {
                 chaseTimeCounter -= Time.deltaTime;
@@ -35,22 +41,71 @@ public class EnemyController : MonoBehaviour
                     navAgent.destination = startPosition;
                 }
             }
+
+            if (navAgent.remainingDistance < 0.25f)
+            {
+                enemyAnimator.SetBool("isRunning", false);
+            }
+            else
+            {
+                enemyAnimator.SetBool("isRunning", true);
+            }
         }
         else
         {
             if (Vector3.Distance(transform.position, targetPointPosition) > distanceToStop)
             {
                 navAgent.destination = targetPointPosition;
+                enemyAnimator.SetBool("isRunning", true);
             }
             else
             {
                 navAgent.destination = transform.position;
+                enemyAnimator.SetBool("isRunning", false);
             }
+
             if (Vector3.Distance(transform.position, targetPointPosition) > minDistanceForLose)
             {
                 isChasing = false;
                 chaseTimeCounter = keepChasingTime;
             }
+
+            fireCounter -= Time.deltaTime;
+            if (fireCounter <= 0 && canFire)
+            {
+                fireCounter = fireRate;
+
+                bulletPoint.LookAt(targetPointPosition + new Vector3(0f, .5f, 0f));
+                Vector3 targetDirection = targetPointPosition - transform.position;
+                float angle = Vector3.SignedAngle(targetDirection, transform.forward, Vector3.up);
+
+                if (Mathf.Abs(angle) < 30f)
+                {
+                    enemyAnimator.SetBool("isRunning", false);
+                    navAgent.destination = transform.position;
+                    enemyAnimator.SetTrigger("Shoot");
+                    Instantiate(bullet, bulletPoint.position, bulletPoint.rotation);
+                    burstLimiter++;
+
+                    if (burstLimiter > 5)
+                    {
+                        canFire = false;
+                        StartCoroutine(BurstCooldown());
+                    }
+                }
+                else
+                {
+                    StartCoroutine(BurstCooldown());
+                }
+            }
         }
+    }
+
+    IEnumerator BurstCooldown()
+    {
+        yield return new WaitForSeconds(3f);
+        enemyAnimator.SetBool("isRunning", true);
+        burstLimiter = 0;
+        canFire = true;
     }
 }
